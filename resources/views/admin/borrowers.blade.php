de2140ca685093ad23a5b0f8816c51e43cf278e0
Yoosuf Sayyid <cena26657@gmail.com>
2022-12-06 13:00:27 +0500

    k

--- resources/views/admin/borrowers.blade.php
+++ resources/views/admin/borrowers.blade.php
@@ -52,7 +52,6 @@
                 </button>
               </h2>
               
-
               <div id="flush-collapse{{$borrower->id}}" class="accordion-collapse collapse" style="border: none;" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
                 <div class="accordion-body p-0 pt-4" style="width: 1248px">
                   <div class="accordion-inner">
